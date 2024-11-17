26d71da07af2accfa2ec7b2c47fa2b0ed1c81592
Zuzana Čechová <zu.cechova9@gmail.com>
2024-11-17 16:29:10 +0100

    opravene nazvy cateringu

--- script.js
+++ script.js
@@ -21,11 +21,11 @@ const pocetLidi = Number(prompt("Pro kolik lidí?"))
 const catering = prompt("Jakou cateringovou společnost jste si vybrali? \n 1. Just Food: Dodává levně. \n 2. Your Mama: Dodává středně drahé jídlo z lepších surovin. \n 3. Flavour Haven: Dodává exkluzivní jídlo z nejkvalitnějších surovin. \n (Zadejte čislo nebo název společnosti) ")
 
 const creatEvent = (udalost, pocetLidi, catering) => {
-    if (catering.toLowerCase() === "food" || catering === "1") {
+    if (catering.toLowerCase() === " just food" || catering === "1") {
        return ` <p> Na událost ${udalost} dodá jídlo: ${justFood(pocetLidi)}`
-    } else if (catering.toLowerCase() === "mama" || catering === "2") {
+    } else if (catering.toLowerCase() === " your mama" || catering === "2") {
        return ` <p> Na událost ${udalost} dodá jídlo: ${yourMama(pocetLidi)}`
-    } else if ( catering.toLowerCase() === "haven" || catering === "3") {
+    } else if ( catering.toLowerCase() === "flavour haven" || catering === "3") {
       return  ` <p> Na událost ${udalost} dodá jídlo: ${flavourHaven(pocetLidi)}`
 } else {return "Něco se pokazilo, zkuste to prosím znovu."}
 } 
